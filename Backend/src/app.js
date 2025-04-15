d85724cd8fdbc77b03d6d011c494097830ddedfd
Sherin Jebamalar <159452844+Sherin-Jebamalar-M@users.noreply.github.com>
2025-04-16 01:01:03 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,27 +2,25 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import passport from "passport";
+import dotenv from "dotenv"; // Import dotenv if you're using it here
+
+dotenv.config(); // Load environment variables if needed in this file
 
 const app = express();
 
+const frontendURL = process.env.FRONTEND_URL || "http://localhost:5173";
+
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+    cors({
+        origin: frontendURL, // Use the environment variable for the frontend URL
+        credentials: true,
+    })
 );
 
 app.use(express.json({ limit: "16kb" })); // to parse json in body
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // to parse url
 app.use(express.static("public")); // to use static public folder
 app.use(cookieParser()); // to enable CRUD operation on browser cookies
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  // Add other CORS headers as needed
-  next();
-});
 
 // Passport middleware
 app.use(passport.initialize());
