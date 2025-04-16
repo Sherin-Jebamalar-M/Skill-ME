f7f8a2f625da7b92223adb25a64fef97e3415c71
Sherin Jebamalar <159452844+Sherin-Jebamalar-M@users.noreply.github.com>
2025-04-16 11:05:19 +0530

    Update PrivateRoutes.jsx

--- Frontend/src/util/PrivateRoutes.jsx
+++ Frontend/src/util/PrivateRoutes.jsx
@@ -1,8 +1,24 @@
 import { Outlet, Navigate } from "react-router-dom";
 
+const getAccessToken = () => {
+  const name = "accessToken=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+};
+
 const PrivateRoutes = () => {
-  const userInfo = localStorage.getItem("userInfo");
-  return userInfo ? <Outlet /> : <Navigate to="/login" />;
+  const token = getAccessToken();
+  return token ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoutes;
